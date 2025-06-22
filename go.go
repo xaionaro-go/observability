24b7017284e4cd164a2300877d165e2a4b98fa49
Dmitrii Okunev <xaionaro@dx.center>
2025-06-22 14:09:56 +0100

    Add GoSafeRestartable

--- go.go
+++ go.go
@@ -23,18 +23,7 @@ func CallSafe(ctx context.Context, fn func(context.Context)) {
 func Go(ctx context.Context, fn func(context.Context)) {
 	if EnableGoroutineTracing {
 		stack := debug.Stack()
-		belt.GetFields(ctx).ForEachField(func(f *field.Field) bool {
-			if f.Key == "goroutines_stack" {
-				prevStack, ok := f.Value.([]byte)
-				if !ok {
-					return true
-				}
-				stack = append(stack, prevStack...)
-				return false
-			}
-			return true
-		})
-		ctx = belt.WithField(ctx, "goroutines_stack", string(stack))
+		ctx = addGoroutinesStack(ctx, stack)
 	}
 	go Call(ctx, fn)
 }
@@ -42,18 +31,39 @@ func Go(ctx context.Context, fn func(context.Context)) {
 func GoSafe(ctx context.Context, fn func(context.Context)) {
 	if EnableGoroutineTracing {
 		stack := debug.Stack()
-		belt.GetFields(ctx).ForEachField(func(f *field.Field) bool {
-			if f.Key == "goroutines_stack" {
-				prevStack, ok := f.Value.([]byte)
-				if !ok {
-					return true
-				}
-				stack = append(stack, prevStack...)
-				return false
-			}
-			return true
-		})
-		ctx = belt.WithField(ctx, "goroutines_stack", string(stack))
+		ctx = addGoroutinesStack(ctx, stack)
 	}
 	go CallSafe(ctx, fn)
+}
+
+func GoSafeRestartable(ctx context.Context, fn func(context.Context)) {
+	if EnableGoroutineTracing {
+		stack := debug.Stack()
+		ctx = addGoroutinesStack(ctx, stack)
+	}
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+			CallSafe(ctx, fn)
+		}
+	}()
+}
+
+func addGoroutinesStack(ctx context.Context, stack []byte) context.Context {
+	belt.GetFields(ctx).ForEachField(func(f *field.Field) bool {
+		if f.Key == "goroutines_stack" {
+			prevStack, ok := f.Value.([]byte)
+			if !ok {
+				return true
+			}
+			stack = append(stack, prevStack...)
+			return false
+		}
+		return true
+	})
+	return belt.WithField(ctx, "goroutines_stack", string(stack))
 }
